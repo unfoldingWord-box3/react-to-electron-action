5b8f1721c877fcb07250d619d99f439d7580ed88
mandolyte <cecil.new@gmail.com>
2020-04-02 09:37:34 -0400

    setOutputs

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-} catch (error) {
+  core.setOutput("appname", appName);
+  core.setOutput("appid", appId);
+  core.setOutput("path", path);
+  } catch (error) {
   core.setFailed(error.message);
 }
 
