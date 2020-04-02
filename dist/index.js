ca0ae2e9fd218af0d97a36f04b9ad6b002b0f686
mandolyte <cecil.new@gmail.com>
2020-04-02 09:40:58 -0400

    dist updated

--- dist/index.js
+++ dist/index.js
@@ -1389,16 +1389,26 @@ const github = __webpack_require__(30);
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const appName = core.getInput('appname');
+  console.log(`App name is ${appName}!`);
+  const appId = core.getInput('appid');
+  console.log(`App id is ${appId}!`);
+  const path = core.getInput('public-url');
+  console.log(`Path is ${path}!`);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-} catch (error) {
+  core.setOutput("appname", appName);
+  core.setOutput("appid", appId);
+  core.setOutput("path", path);
+  } catch (error) {
   core.setFailed(error.message);
 }
+
+/*
+const time = (new Date()).toTimeString();
+core.setOutput("time", time);
+*/
 
 /***/ }),
 
